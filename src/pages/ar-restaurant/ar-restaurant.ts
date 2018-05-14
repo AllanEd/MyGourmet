786aba659ac78bacbd98e803401070988f0ea8e0
Your Name <lana.sattelmaier@stud.hs-ruhrwest.de>
2018-05-14 14:49:57 +0200

    test

--- src/pages/ar-restaurant/ar-restaurant.ts
+++ src/pages/ar-restaurant/ar-restaurant.ts
@@ -1,12 +1,8 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import {ARController, ARThreeScene, artoolkit } from 'jsartoolkit5';
+//import { WebGLRenderer } from 'three';
 
-/**
- * Generated class for the ArRestaurantPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,24 +10,74 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ar-restaurant.html',
 })
 export class ArRestaurantPage {
-  @ViewChild('videoElement') videoElement: any;
-  video: any;
+  @ViewChild('canvas') canvas: any;
+  //@ViewChild('videoElement') videoElement: any;
+  //video: any;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ArRestaurantPage');
-    this.video= this.videoElement.nativeElement;
+    /*this.video= this.videoElement.nativeElement;
+    let constraints = {
+      audio: false,
+      video: {
+        width: 182,
+        height: 320
+      }
+    };
+    this.initCamera(constraints);*/
+
+    //this.initCanvas();
   }
 
-  initCamera(config:any):void {
+ /* initCanvas():void {
+    ARController.getUserMediaThreeScene({
+      maxARVideoSize: 640,
+      facingMode: 'environment',
+      cameraParam: 'assets/data/camera_para.dat',
+      onSuccess: (arScene: ARThreeScene, arController, arCamera) => {
+
+        arController.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_MONO_AND_MATRIX);
+        let renderer = new WebGLRenderer({
+          antialias: true,
+          alpha: true,
+          canvas: this.canvas
+        });
+        this.renderer = renderer;
+
+        if (arController.orentation === 'portrait') {
+          renderer.setSize(this.height, this.width);
+          renderer.domElement.style.transformOrigin = '0 0';
+          renderer.domElement.style.transform = 'rotate(-90deg)translateX(-100%)';
+        } else {
+          renderer.setSize(this.width, this.height);
+        }
+
+        let tick = () => {
+          arScene.process();
+          arScene.renderOn(renderer);
+          requestAnimationFrame(tick);
+        };
+
+        tick();
+        this.trackMarker(arScene, arController, 5, plane);
+
+      }
+    });
+  }
+
+
+ /* initCamera(config:any):void {
     navigator.mediaDevices.getUserMedia(config).then(stream => {
       this.video.srcObject= stream;
       this.video.play();
     }).catch((err)=> {
       console.error(err);
     });
-  }
+  }*/
 
-}
+
+ }
--- src/pages/restaurants-list/restaurants-list.ts
+++ src/pages/restaurants-list/restaurants-list.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
-import 'rxjs/add/operator/map';
-
-//import google;
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 
 
 
-/**
- * Generated class for the RestaurantsListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var google;
+let map: any;
+let infowindow: any;
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
 @IonicPage()
 @Component({
@@ -20,23 +18,72 @@ import 'rxjs/add/operator/map';
   templateUrl: 'restaurants-list.html',
 })
 export class RestaurantsListPage {
-  restaurants: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
-    // google.maps.places.PlacesService(map);
-    this.http.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=AIzaSyD1NJibvMwPG3luDLP-TdJxuXywxsmzgdI ')
-      .subscribe((res) => {console.log(res)});
+  @ViewChild('map') mapElement: ElementRef;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+ platform.ready().then(()=>{
+   this.initMap();
+    });
   }
 
   cameraButton(){
     this.navCtrl.push('ArRestaurantPage');
   }
 
-  ionViewDidLoad() {
+ /* ionViewDidLoad() {
     console.log('ionViewDidLoad RestaurantsListPage');
     navigator.geolocation.getCurrentPosition((pos) => {
       console.log(pos);
+    });
+  }*/
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      console.log(location);
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: location.coords.latitude, lng: location.coords.longitude},
+        zoom: 15
+      });
+
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: {lat: location.coords.latitude, lng: location.coords.longitude},
+        radius: 1000,
+        type: ['store']
+      }, (results,status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+  }
+
+  createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeLoc,
+      icon: image
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      //infowindow.setContent(place.name);
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        'Place ID: ' + place.place_id + '<br>' +
+        place.vicinity + '</div>');
+      infowindow.open(map, this);
     });
   }
 
